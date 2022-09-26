bbee7d02a1b349c05f5dd449b6d5de03de351c46
Jack Reilly <jackdreilly@gmail.com>
2022-09-26 10:38:18 +0200

    region and gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,4 +3,6 @@ import * as functions from "firebase-functions";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onCall(() => "hi");
+export const helloWorld = functions.region("europe-west3").https.onCall(() =>
+  "hi"
+);
